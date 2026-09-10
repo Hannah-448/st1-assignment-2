54eb7db1f19076e20f43e67833fabea37efff64d
Hannah <u3320286@uni.canberra.edu.au>
2026-09-11 09:52:46 +1000

    Week 5 complete

--- week4/smartcare_v01.py
+++ week4/smartcare_v01.py
@@ -15,29 +15,32 @@
 # appointment2_time = '2024-07-20 11:30 AM'
 # print(f"Patient: {patient2_name} | Practitioner: {practitioner2_name} | Time: {appointment2_time}")
 
-
 # Use lists, dictionaries and functions to enhance the Python file
 
-#appointments = []
+appointments = []
 
-#def book_appointment(patient_name, practitioner_name, appointment_time):
-#    if not patient_name:
-#        raise ValueError("Patient name cannot be empty")
-#    appointment = {
-#        "patient": patient_name,
-#        "practitioner": practitioner_name,
-#       "time": appointment_time
-#   }
-#   appointments.append(appointment)
+def book_appointment(patient_name, practitioner_name, appointment_time):
+    if not patient_name:
+        raise ValueError("Patient name cannot be empty")
+    if not practitioner_name:
+        raise ValueError("Practitioner name cannot be empty")
+    if not appointment_time:
+        raise ValueError("Appointment time cannot be empty")
+    appointment = {
+        "patient": patient_name,
+        "practitioner": practitioner_name,
+        "time": appointment_time
+    }
+    appointments.append(appointment)
 
-#def display_appointments():
-#    if not appointments:
-#    print("No appointments recorded.")
-#     return
-#  for appointment in appointments:
-#       print(f"Patient: {appointment['patient']} | Practitioner: {appointment['practitioner']} | Time: {appointment['time']}")
+def display_appointments():
+    if not appointments:
+        print("No appointments recorded.")
+        return
+    for appointment in appointments:
+        print(f"Patient: {appointment['patient']} | Practitioner: {appointment['practitioner']} | Time: {appointment['time']}")
 
-#print("Welcome to SmartCare: The Clinical Appointment Booking System!")
-#book_appointment('Alice Smith', 'Dr. John Doe', '2024-07-20 10:00 AM')
-#book_appointment('Bob Johnson', 'Dr. Jane Roe', '2024-07-20 11:30 AM')
-#display_appointments()
+print("Welcome to SmartCare: The Clinical Appointment Booking System!")
+book_appointment('Alice Smith', 'Dr. John Doe', '2024-07-20 10:00 AM')
+book_appointment('Bob Johnson', 'Dr. Jane Roe', '2024-07-20 11:30 AM')
+display_appointments()
